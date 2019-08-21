54406f61211927d4885e483096e94fb2eacb6383
Jim McManus <jmpmcmanus@flatapps.net>
2019-08-21 14:20:28 -0400

    revision 08/21/19

--- django-pfiProject-docker/pfiProject/secrets/secrets.py
+++ django-pfiProject-docker/pfiProject/secrets/secrets.py
@@ -6,5 +6,5 @@
 # You must uncomment, and set SECRET_KEY to a secure random value
 # e.g. https://djskgen.herokuapp.com/
 
-SECRET_KEY = '$4ni)mi!d8+th0%i$#im9go59na%$nplk2=&$#g$t$fmynxc22'
-GOOGLE_MAP_API_KEY = 'AIzaSyAS4zMD8L-HCOT_QsXGt3BImGscB93BF5U'
+SECRET_KEY = 'xxxxxxxxxxxxxxx'
+GOOGLE_MAPS_API_KEY = 'xxxxxxxxxxxxxxxx'
