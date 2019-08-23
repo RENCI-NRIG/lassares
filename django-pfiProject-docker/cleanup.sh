ce40fc5af5463a807ca1ab3d61af9c53cab710c5
Jim McManus <jmpmcmanus@flatapps.net>
2019-08-23 08:38:33 -0400

    added volume rm to cleanup.sh, and db view creation to run_uwsgi.sh

--- django-pfiProject-docker/cleanup.sh
+++ django-pfiProject-docker/cleanup.sh
@@ -3,7 +3,8 @@ docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
 docker rmi -f $(docker images -q)
 docker network rm $(docker network ls --format "{{.ID}}")
+docker volume rm $(docker volume ls -qf dangling=true)
 #mvn clean install
 rm -rf .venv
 find . -name __pycache__ | xargs rm -rf
-rm -rf static/admin/ static/geoposition/
+rm -rf static/admin/ static/gis  static/mapwidgets  static/rest_framework 
--- django-pfiProject-docker/run_uwsgi.sh
+++ django-pfiProject-docker/run_uwsgi.sh
@@ -7,6 +7,8 @@ python manage.py migrate
 #python manage.py createsuperuser --username admin --email komal.thareja@gmail.com --noinput
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'komal.thareja@gmail.com', '$ADMIN_PWD')" | python manage.py shell
 python manage.py collectstatic --noinput
+#create db view
+echo -e "CREATE VIEW drf_data_testdatav_model AS SELECT fid AS id, device_id, timestamp, job_id, chem_id FROM drf_data_testdata_model;\nCREATE VIEW drf_data_timestamp_model AS SELECT timestamp as id, timestamp as label FROM drf_data_testdata_model;\nCREATE VIEW drf_data_jobid_model AS SELECT DISTINCT job_id as id, job_id as label FROM drf_data_testdata_model;" | python manage.py dbshell
 
 if [[ "${USE_DOT_VENV}" -eq 1 ]]; then
     if [[ "${RUN_ROOT}" -eq 1 ]]; then
