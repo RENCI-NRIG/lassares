26b386f1376a29c2987ee89c0a3a0b7a7c801832
Jim McManus <jmpmcmanus@flatapps.net>
2019-11-08 16:42:28 -0500

    modified openssl command

--- django-pfiProject-docker/generate-certificates.sh
+++ django-pfiProject-docker/generate-certificates.sh
@@ -11,7 +11,7 @@ fi
 cd $ROOT_DIR/certs
 openssl req -x509 -newkey rsa:2048 -sha256 -days 3650 -nodes \
   -keyout self.signed.key -out self.signed.crt \
-  -subj "/C=US/ST=North Carolina/L=Chapel Hill/O=RENCI/OU=NRIG/CN=example.com/emailAddress=jmpmcman@renci.org/" \
+  -subj "/C=US/ST=North Carolina/L=Chapel Hill/O=RENCI/OU=NRIG/CN=example.com/emailAddress=jmpmcman@renci.org" \
   -addext "subjectAltName=DNS:example.com,DNS:example.com,IP:10.0.0.1"
 cd -
 
